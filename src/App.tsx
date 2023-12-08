92abf1ade72c53fdd760e1805955b91eeb54f486
sachem <karol.padiasek@gmail.com>
2023-12-08 16:58:39 +0000

    android

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,6 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import { Capacitor } from '@capacitor/core';
-/* Tailwind styles */
-// import './theme/tailwind.css';
 setupIonicReact();
 
 export default function App() {
